f93c20c9edd6ab86d5f21dcc55f3a9521bbcd9e5
Arjun Agarwal <142018342+v659@users.noreply.github.com>
2025-12-09 18:50:13 +0530

    Refactor Vector2Math initialization and methods

--- vector2D.py
+++ vector2D.py
@@ -1,6 +1,4 @@
 import math
-import numpy
-
 
 class Vector2Math:
     """
@@ -8,64 +6,34 @@ class Vector2Math:
     All angles are in degrees, conversions done inside code.
     """
 
-    def __init__(self, x_component, y_component):
-        self.x = x_component
-        self.y = y_component
+    def __init__(self, decimals, x=None, y=None, angle=None, hypot=None):
+        self.DECIMALS = decimals
+        if angle is not None and hypot is not None:
+            self.x = round(hypot * math.cos(math.radians(angle)), self.DECIMALS)
+            self.y = round(hypot * math.sin(math.radians(angle)), self.DECIMALS)
+        elif x is not None and y is not None:
+            self.x = round(x, self.DECIMALS)
+            self.y = round(y, self.DECIMALS)
+        else:
+            raise ValueError("Must provide either x and y, or angle and hypot")
 
-    def get_vector(self):
+    @property
+    def vector(self):
         return (self.x, self.y)
 
     @property
     def magnitude(self):
-        return self.pythagorean_theorem(self.x, self.y)
+        return round(math.sqrt(self.x**2 + self.y**2), self.DECIMALS)
 
     @property
     def angle(self):
-        return self.calculate_angle(self.x, self.y)
-
-    @property
-    def vector(self):
-        return self.get_vector()
-    @staticmethod
-    def pythagorean_theorem(x_component, y_component):
-        return math.sqrt(x_component**2 + y_component**2)
-
-    @staticmethod
-    def calculate_xy_components(angle, hypot):
-        angle = math.radians(angle)
-        y_component = hypot * math.sin(angle)
-        x_component = hypot * math.cos(angle)
-        return x_component, y_component
-
-    @staticmethod
-    def calculate_hypot(x_component, y_component):
-        return Vector2Math.pythagorean_theorem(x_component, y_component)
-
-    @staticmethod
-    def calculate_angle(x_component, y_component):
-        return math.degrees(math.atan2(y_component, x_component))
-
-    @classmethod
-    def from_angle_and_hypot(cls, angle, hypot):
-        x, y = cls.calculate_xy_components(angle, hypot)
-        return cls(x, y)
-
-    @classmethod
-    def from_points(cls, x1, y1, x2, y2):
-        return cls(x2 - x1, y2 - y1)
+        return round(math.degrees(math.atan2(self.y, self.x)), self.DECIMALS)
 
     def __add__(self, b):
-        return Vector2Math(self.x + b.x, self.y + b.y)
+        return Vector2Math(self.DECIMALS, round(self.x + b.x, self.DECIMALS), round(self.y + b.y, self.DECIMALS))
 
     def __sub__(self, b):
-        return Vector2Math(self.x - b.x, self.y - b.y)
+        return Vector2Math(self.DECIMALS, round(self.x - b.x, self.DECIMALS), round(self.y - b.y, self.DECIMALS))
 
     def __repr__(self):
         return f"Vector2Math({self.x}, {self.y})"
-
-
-a = Vector2Math(8.7, 5)
-b = Vector2Math(0, 5)
-c = a - b
-print(c.vector)
-
